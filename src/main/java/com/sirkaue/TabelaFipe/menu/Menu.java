9142116096fb5fe831f155f016e8415be71482c2
sirkaue <kauekdslopes@gmail.com>
2024-05-28 14:59:32 -0300

    Buscando todas as avaliações por veículo.

--- src/main/java/com/sirkaue/TabelaFipe/menu/Menu.java
+++ src/main/java/com/sirkaue/TabelaFipe/menu/Menu.java
@@ -2,12 +2,15 @@ package com.sirkaue.TabelaFipe.menu;
 
 import com.sirkaue.TabelaFipe.model.Dados;
 import com.sirkaue.TabelaFipe.model.Modelos;
+import com.sirkaue.TabelaFipe.model.Veiculo;
 import com.sirkaue.TabelaFipe.service.ConsumoApi;
 import com.sirkaue.TabelaFipe.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Menu {
     private ConsumoApi consumoApi = new ConsumoApi();
@@ -30,7 +33,6 @@ public class Menu {
 
         String opcao = sc.nextLine();
         String endereco;
-
 
         if (opcao.toLowerCase().contains("car")) {
             endereco = URL_BASE + "carros/marcas";
@@ -60,5 +62,33 @@ public class Menu {
                 .stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
+
+        System.out.println("\nDigite um trecho do nome do carro a ser buscado: ");
+        String nomeVeiculo = sc.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("\nModelos filtrados");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("\nDigite por favor o código do modelo para buscar os valores de avaliação: ");
+        String codigoModelo = sc.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumoApi.obterDados(endereco);
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < anos.size(); i++) {
+            String enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = consumoApi.obterDados(enderecoAnos);
+            Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("\nTodos os veículos filtrados com avaliações por ano: ");
+        veiculos.forEach(System.out::println);
     }
 }
